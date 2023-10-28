2514c0a589e7a548b31a50755f7c164bf681ebd8
ajith <ajith24ram@gmail.com>
2023-10-28 18:02:07 +0530

    cake reducer

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+const { act } = require('react-dom/test-utils');
+const redux =require('redux')
+
+const createStore = redux.createStore; // extracting creaye store method
+
+
 const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
 function orderCake(){
   return  {
         type: CAKE_ORDERED,
-        quantity: 1
+        payload: 1
     }
-}
+} 
+
+function restockCake(quantity=1){
+    return  {
+          type: CAKE_RESTOCKED,
+          payload: quantity
+      }
+  }
+  
+  // action creater
 
 // an action is an object with the type property
 // action creater is a function . which return the action object
-
 
 // reducer is a function which takes state and action as an arguement and returns a new or next state
 // (prevState,action) => newState
@@ -21,10 +36,46 @@ const reducer  = (state = initialState, action) =>{
         case CAKE_ORDERED:
             return {
                 ...state, // copying the state object using spread operator
-                numberOfCakes : state.numberOfCakes-1
+                numberOfCakes : state.numberOfCakes-action.payload
+            }
+        case CAKE_RESTOCKED:
+            return{
+                ...state,
+                numberOfCakes : state.numberOfCakes + action.payload
             }
         default:
             return state
     }
 
 }
+
+const store = createStore(reducer) // creating store 
+
+console.log("initial state",store.getState()) // getting state of the store
+
+const unSubscribe = store.subscribe(()=>console.log("updated state", store.getState())) // subcribing the store to the listen state change 
+
+store.dispatch(orderCake())
+store.dispatch(orderCake()) 
+store.dispatch(orderCake())
+store.dispatch(restockCake(3))
+
+// binding action createra
+const bindActionCreators = redux.bindActionCreators
+
+const actions = bindActionCreators({orderCake, restockCake},store.dispatch)
+
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
+
+
+
+
+unSubscribe() //unsubcribing the store 
+
+
+
+
+
